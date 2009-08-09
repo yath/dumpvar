542a196076bf0d9d571ab1903de7e973a0d3576a
Sebastian Schmidt <yath@yath.de>
2009-08-09 18:55:00 +0200

    Added some documentation

--- Dumpvar.pm
+++ Dumpvar.pm
@@ -6,6 +6,30 @@
 #   Sebastian Schmidt
 # ----------------------------------------------------------------------------
 
+=head1 NAME
+
+Dumpvar - An interface to L<Devel::Peek> that returns a scalar
+
+=head1 SYNOPSIS
+
+    use Dumpvar;
+    (...)
+    my $ret = Dumpvar $foobar;
+
+=head1 DESCRIPTION
+
+Uses L<Devel::Peek>'s Dump to dump perl's internal representation of a
+variable. There's no real magic involved here, the advantage over
+C<Devel::Peek::Dump> is just that C<Dumpvar()> returns a scalar with 
+C<Devel::Peek::Dump>'s output. That makes debugging handier if you
+don't have a usable STDERR.
+
+=head1 SEE ALSO
+
+L<Devel::Peek>, L<perlguts>,
+L<http://www.perl.org/tpc/1998/Perl_Language_and_Modules/Perl%20Illustrated/>
+=cut
+
 package Dumpvar;
 require Exporter;
 @ISA = qw(Exporter);
